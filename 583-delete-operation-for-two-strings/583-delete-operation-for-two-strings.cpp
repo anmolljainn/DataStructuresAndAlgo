573902842b110f746ad7a89f0c042a12fe380d89
anmolljainn <89248117+anmolljainn@users.noreply.github.com>
2022-06-14 09:42:55 +0530

    Time: 15 ms (82.94%), Space: 7.6 MB (71.41%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    int findLCS(string &str1, string &str2, int n, int m)
+{
+    int dp[n + 1][m + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (str1[i - 1] == str2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size(),m=word2.size();
+        return n+m-2*(findLCS(word1,word2,n,m));
+    }
+};
